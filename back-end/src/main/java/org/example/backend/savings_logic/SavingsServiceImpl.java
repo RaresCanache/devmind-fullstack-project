3e58b6b2c7c0c6996979ffd815a194cd83fbf24b
Rares Canache <rares.canache2001@gmail.com>
2025-02-14 01:28:22 +0200

    Changed from repository injection to service injection in SavingsServiceImpl class

--- back-end/src/main/java/org/example/backend/savings_logic/SavingsServiceImpl.java
+++ back-end/src/main/java/org/example/backend/savings_logic/SavingsServiceImpl.java
@@ -3,14 +3,15 @@ package org.example.backend.savings_logic;
 import lombok.RequiredArgsConstructor;
 import org.example.backend.exception_handlers.BankAccountNotFoundException;
 import org.example.backend.exception_handlers.FinancialPlanNotFoundException;
-import org.example.backend.exception_handlers.UserNotFoundException;
 import org.example.backend.models.BankAccount;
-import org.example.backend.models.Expense;
 import org.example.backend.models.FinancialPlan;
 import org.example.backend.repositories.BankAccountRepository;
 import org.example.backend.repositories.ExpenseRepository;
 import org.example.backend.repositories.FinancialPlanRepository;
-import org.example.backend.repositories.UserRepository;
+import org.example.backend.service_interface.BankAccountService;
+import org.example.backend.service_interface.ExpenseService;
+import org.example.backend.service_interface.FinancialPlanService;
+import org.example.backend.service_interface.UserService;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -22,10 +23,10 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class SavingsServiceImpl implements SavingsService {
-    private final UserRepository userRepository;
-    private final FinancialPlanRepository financialPlanRepository;
-    private final ExpenseRepository expenseRepository;
-    private final BankAccountRepository bankAccountRepository;
+    private final UserService userService;
+    private final FinancialPlanService financialPlanService;
+    private final ExpenseService expenseService;
+    private final BankAccountService bankAccountService;
 
     //Here I used ChronoUnit for plans that span over more than 1 year
     private long noOfDaysFinancialPlan(LocalDate startDate, LocalDate endDate) {
@@ -34,21 +35,18 @@ public class SavingsServiceImpl implements SavingsService {
 
     //Used Lambda expressions for adding all expense amounts
     private BigDecimal totalExpenses(Integer userId) {
-        return expenseRepository.findAllByUser_Id(userId)
+        return expenseService.getAllExpensesByUserId(userId)
                 .stream()
                 .map(expense -> expense.getAmount())
                 .reduce(BigDecimal.ZERO, (total, amount) -> total.add(amount));
     }
 
     public List<Integer> computeAmountPerDayForUserIdAndBankAccountId(Integer userId, Integer bankAccountId) {
-        if (!userRepository.existsById(userId)) {
-            throw new UserNotFoundException("User with id: " + userId + " not found");
-        }
-        FinancialPlan financialPlan = financialPlanRepository.findByUser_Id(userId)
-                .orElseThrow(() -> new FinancialPlanNotFoundException("Financial plan not existent"));
+        userService.userExistsById(userId);
 
-        BankAccount bankAccount = bankAccountRepository.findById(bankAccountId)
-                .orElseThrow(() -> new BankAccountNotFoundException("Bank account with id:" + bankAccountId + " not found"));
+        FinancialPlan financialPlan = financialPlanService.getFinancialPlanById(userId);
+
+        BankAccount bankAccount = bankAccountService.getBankAccountById(bankAccountId);
 
         List<Integer> amountRequiredPerDay = new ArrayList<>();
         LocalDate startDate = financialPlan.getStartDate();
@@ -58,7 +56,7 @@ public class SavingsServiceImpl implements SavingsService {
 
         BigDecimal startingBalance = bankAccount.getBalance().subtract(totalExpenses(userId));
 
-
+        System.out.println(startingBalance);
         return amountRequiredPerDay;
     }
 
