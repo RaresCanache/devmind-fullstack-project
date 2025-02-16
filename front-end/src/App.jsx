e0f5a9c9d3838da0646c8d05418a5441ded6d387
Rares Canache <rares.canache2001@gmail.com>
2025-02-17 01:11:56 +0200

    Created working HomeScreen component

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,15 +1,17 @@
 import './App.css'
 import {Route, Routes} from "react-router";
+import HomeScreen from "./components/HomeScreen.jsx";
 
 function App() {
 
-  return (
-    <>
-      <Routes>
-
-      </Routes>
-    </>
-  )
+    return (
+        <>
+            <Routes>
+                <Route path="" element={<HomeScreen/>}/>
+                <Route path="login"/>
+            </Routes>
+        </>
+    );
 }
 
 export default App
--- front-end/src/components/HomeScreen.jsx
+++ front-end/src/components/HomeScreen.jsx
@@ -1,7 +1,22 @@
-const HomeScreen = () => {
+import "./HomeScreen.css";
+import {useNavigate} from "react-router";
 
+const HomeScreen = () => {
+    const navigate = useNavigate();
 
     return (
-
-    )
+        <div className="container">
+            <h1 style={{fontSize: "60px"}}>Welcome to my app</h1>
+            <div>
+                <button onClick={() => navigate("login")}>
+                    Login
+                </button>
+                <button onClick={() => navigate("register")}>
+                    Register
+                </button>
+            </div>
+        </div>
+    );
 };
+
+export default HomeScreen;
